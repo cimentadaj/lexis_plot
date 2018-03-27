3676ec11c8189180c787ad7f4413c7affed96482
cimentadaj <cimentadaj@gmail.com>
2018-03-27 20:39:57 +0200

    Add first year in x axis

--- original_attempt.R
+++ original_attempt.R
@@ -42,14 +42,18 @@ library(tidyverse)
 # Load population data
 
 # replace with your human mortaility accounty
+# id <- read_rds("id")
 id <- c("email", "password")
-country <- "DNK"
+country <- "SWE"
 # For width reference
 # If it's set to NA, the
 # width is relative to that cohorts
 # maximum pop
 selected_year <- NA
-factor <- 0.95
+
+# Choose Male (1) of Female (2)
+ch <- 1
+
 
 pop <-
   readHMDweb(CNTRY = country,
@@ -99,9 +103,6 @@ choose <- c("Male","Female")
 export <- c("MALES","FEMALES")
 title <- c("Males","Females")
 
-# Choose Male (1) of Female (2)
-ch <- 1
-
 # Choose data from time1 to time2
 time <- sort(unique(pop_long$Year))
 time1 <- time[1]
@@ -131,6 +132,7 @@ if (!is.na(selected_year)) {
   selected_year_max <- pop_ch$Maxpop
 }
 
+factor <- 0.95
 pop_ch$relative_pop <- pop_ch$Pop/selected_year_max*factor
 
 # popstand <- pop_ch$Pop/max(pop_ch$Pop)*factor
@@ -227,15 +229,22 @@ shrink_fun <- function(x, shrink, x_value = TRUE) {
 
 par(bg = "black", mar=c(5, 4, 4, 2),fig=c(0,1,0,1))
 
-plot(x = c(coh[1], coh[length(coh)]),
+plot(x = c(time1, time2),
      y = c(ages[1], ages[length(ages)]),
      pch=20,
-     col="transparent", col.axis=alpha("grey95",0.75),
-     font.lab=2, cex.lab=1.2, xlab="Year", ylab="Age",
-     xlim=c(time1, time2), col.lab=alpha("grey95",0.75))
+     col="transparent",
+     col.axis=alpha("grey95",0.75),
+     font.lab=2,
+     cex.lab=1.2,
+     ylab="Age",
+     xlab="Year",
+     col.lab=alpha("grey95",0.75),
+     xaxt = "n")
 
-# title(main=paste("Males"," (Sweden) - Cohort Mortality Rates",sep=""),
-#       col.main=alpha("grey95",0.75))
+axis(1, at = seq(time1, time2, 50), xlab = "Year", col.axis = alpha("grey95", 0.75))
+
+title(main=paste(title[ch]," ", country, " - Cohort Mortality Rates",sep=""),
+      col.main=alpha("grey95",0.75))
 
 # You sort of fixed the colors but you still need to figure out how to change
 # the border color of the polygons and the first line of colors.
@@ -315,189 +324,189 @@ text(0.35,0.5,sprintf("%1.0f",1),col=alpha("grey95",0.75))
 # dev.off()
 
 ## selected data
-country <- "SWE"
-
-population <-
-  readHMDweb(CNTRY = country,
-             item = "Population",
-             username = id[1],
-             password = id[2]) %>%
-  select(Year, Age, Female1, Male1) %>%
-  rename(Female = Female1,
-         Male = Male1) %>%
-  arrange(Year, Age) %>%
-  filter(between(Age, 0, 100), between(Year, 1751, 2011))
-
-death <-
-  readHMDweb(CNTRY = country,
-             item = "Deaths_1x1",
-             username = id[1],
-             password = id[2]) %>%
-  select(Year, Age, Female, Male, Total) %>%
-  arrange(Year, Age)
-
-exp_death <-
-  readHMDweb(CNTRY = country,
-             item = "Exposures_1x1",
-             username = id[1],
-             password = id[2]) %>%
-  select(-OpenInterval)
-
-death_matrix <-
-  death %>%
-  select(Year, Age, Female) %>%
-  filter(between(Age, 0, 100), between(Year, 1751, 2011)) %>%
-  spread(Year, Female) %>%
-  as.matrix() %>%
-  .[, -1]
-
-rownames(death_matrix) <- 0:100
-
-exp_matrix <-
-  exp_death %>%
-  select(Year, Age, Female) %>%
-  filter(between(Age, 0, 100), between(Year, 1751, 2011)) %>%
-  spread(Year, Female) %>%
-  as.matrix() %>%
-  .[, -1]
-
-rownames(exp_matrix) <- 0:100
-
-ages_unique <- death_matrix %>% row.names %>% as.numeric
-years_unique <- death_matrix %>% colnames %>% as.numeric
-
-smooth_calculator <- function(row_unique, col_unique, counts, exposure) {
-  
-  pre_smoothed <- Mort2Dsmooth(x=row_unique, y=col_unique,
-                               Z = counts, offset = log(exposure))
-  
-  mx <- pre_smoothed$fitted.values/exposure
-  mx.2 <- mx[, -1]
-  mx.1 <- mx[, -(ncol(mx))]
-  aai <- 100 * -log(mx.2/mx.1)
-  
-  aai
-}
-
-smoothed_data <- smooth_calculator(ages_unique, years_unique, death_matrix, exp_matrix)
-
-coh <- years_unique
-n_coh <- length(years_unique[-1])
-n_ages <- length(ages_unique)
-
-width_matrix <-
-  population %>%
-  select(-Male) %>%
-  mutate(Female = rescale(Female, c(0, 2))) %>%
-  spread(Age, Female) %>%
-  mutate(Cohort = years_unique) %>%
-  select(Cohort, everything(), -Year) %>%
-  as.matrix()
-
-stacked_df <-
-  smoothed_data %>%
-  as_tibble %>%
-  gather(year, value) %>%
-  mutate(age = rep(ages_unique, length(years_unique[-1])))
-
-
-library(viridis)
-library(classInt)
-colpal <- magma(100, alpha = 1, begin = 0.1, end = 1)
-# Choose bins
-bins <- seq(0, 10)
-# Assigns color according to fixed breaks categorization
-catg <- classIntervals(stacked_df$value, fixedBreaks=bins, style = "fixed")
-color <- findColours(catg, colpal)
-
-color_matrix <-
-  stacked_df %>%
-  mutate(color = color) %>%
-  select(year, age, color) %>%
-  spread(age, color) %>%
-  as.matrix()
-
-shrink_fun <- function(x, shrink, x_value = TRUE) {
-  
-  if(x_value) {
-    xman <- x
-    xman[1] <- mean(x[1:2])-(x[2] - x[1])*(shrink/2)
-    xman[2] <- mean(x[1:2])+(x[2] - x[1])*(shrink/2)
-  } else {
-    xman <- x
-    xman[3] <- mean(x[3:4])-(x[4] - x[3])*(shrink/2)
-    xman[4] <- mean(x[3:4])+(x[4] - x[3])*(shrink/2)
-  }
-  xman
-}
-
-# The next thing to do is incorporate this smoothe data in the previous plot. This
-# shouldn't be that hard given that the data is in the same format (matrix, age x year)
-# as in the previous plot.
-
-# Other thing to do: link both smoothed and previous analysis
-# to common variables so that both things are estimated from the same
-# variables.
-
-par(bg = "black", mar=c(5, 4, 4, 2),fig=c(0,1,0,1))
-
-plot(x = c(years_unique[1], years_unique[length(years_unique)]),
-     y = c(ages_unique[1], ages_unique[length(ages_unique)]),
-     pch=20,
-     col="transparent", col.axis=alpha("grey95",0.75),
-     font.lab=2, cex.lab=1.2, xlab="Year", ylab="Age",
-     xlim=c(years_unique[1], years_unique[length(years_unique)]), col.lab=alpha("grey95",0.75))
-
-# Loop for cohorts
-for (i in 1:n_coh) {
-  # In order to fixate point 2 which we are
-  # are not shrinking
-  mid_x <- seq(coh[i],coh[i]+n_ages,1)
-  mid_y <- c(0:n_ages-1)
-  
-  # Loop for ages
-  for (j in 2:n_ages) {
-    # Lower Lexis triangle
-    x <- c(mid_x[j], mid_x[j]+1, mid_x[j]+1, mid_x[j]+1)
-    y <- c(mid_y[j], mid_y[j], mid_y[j],mid_y[j]+1)
-    
-    x_sh <- shrink_fun(x, width_matrix[i, j])
-    y_sh <- shrink_fun(y, width_matrix[i, j], x_value = F)
-    
-    polygon(x_sh, y_sh, lty=0,col=adjustcolor("grey",alpha.f=0.5), border = adjustcolor("grey",alpha.f=0.5))
-    polygon(x_sh, y_sh, lty=0,col=color_matrix[i, j], border = color_matrix[i, j])
-    
-    # Upper Lexis triangle year + 1
-    x_inv <- c(x[2], x[2] , x[2] ,x[2] + (x[2] - x[1]))
-    y_inv <- c(y[1],y[4],y[4],y[4])
-    
-    x_inv_sh <- shrink_fun(x_inv, width_matrix[i, j], x_value = F)
-    y_inv_sh <- shrink_fun(y_inv, width_matrix[i, j])
-    
-    polygon(x_inv_sh, y_inv_sh, lty=0, col=adjustcolor("grey",alpha.f=0.5), border = adjustcolor("grey",alpha.f=0.5))
-    polygon(x_inv_sh, y_inv_sh, lty=0, col=color_matrix[i, j], border = color_matrix[i, j])
-  }
-}
-
-abline(h=c(seq(0,100,10)),col=alpha("grey95",0.5),lty=2)
-abline(v=c(seq(1750,2010,10)),col=alpha("grey95",0.5),lty=2)
-op1 <- par(mar=c(0,0,0,0), fig=c(0.585,0.7,0.035,0.09), new = TRUE)
-# mtext("Cohort death rates",side=1,line=2,col=alpha("grey95",0.75))
-plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
-#text(0.5,0.5,"Cohort mortality rates (cmx)",col=alpha("grey95",0.75))
-op2 <- par(mar=c(0,0,0,0), fig=c(0.7,0.9,0.05,0.075), new = TRUE)
-plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
-lbi <- length(bins)-1
-for (i in 1:lbi) {
-  rect(bins[i],0,bins[i+1],1,lty=0,col=colpal[i])
-}
-rect(0,0,1,1,lty=1,border=alpha("grey95",0.75))
-op3 <- par(mar=c(0,0,0,0), fig=c(0.70,0.71,0.020,0.045), new = TRUE)
-plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
-text(0.68,0.5,sprintf("%1.0f",0),col=alpha("grey95",0.75))
-op3 <- par(mar=c(0,0,0,0), fig=c(0.885,0.905,0.020,0.045), new = TRUE)
-plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
-text(0.35,0.5,sprintf("%1.0f",1),col=alpha("grey95",0.75))
-
-# Still need to fix legend scale
-# Make years automatic because right now you're filtering years manually when reading the dataset.
+# country <- "SWE"
+# 
+# population <-
+#   readHMDweb(CNTRY = country,
+#              item = "Population",
+#              username = id[1],
+#              password = id[2]) %>%
+#   select(Year, Age, Female1, Male1) %>%
+#   rename(Female = Female1,
+#          Male = Male1) %>%
+#   arrange(Year, Age) %>%
+#   filter(between(Age, 0, 100), between(Year, 1751, 2011))
+# 
+# death <-
+#   readHMDweb(CNTRY = country,
+#              item = "Deaths_1x1",
+#              username = id[1],
+#              password = id[2]) %>%
+#   select(Year, Age, Female, Male, Total) %>%
+#   arrange(Year, Age)
+# 
+# exp_death <-
+#   readHMDweb(CNTRY = country,
+#              item = "Exposures_1x1",
+#              username = id[1],
+#              password = id[2]) %>%
+#   select(-OpenInterval)
+# 
+# death_matrix <-
+#   death %>%
+#   select(Year, Age, Female) %>%
+#   filter(between(Age, 0, 100), between(Year, 1751, 2011)) %>%
+#   spread(Year, Female) %>%
+#   as.matrix() %>%
+#   .[, -1]
+# 
+# rownames(death_matrix) <- 0:100
+# 
+# exp_matrix <-
+#   exp_death %>%
+#   select(Year, Age, Female) %>%
+#   filter(between(Age, 0, 100), between(Year, 1751, 2011)) %>%
+#   spread(Year, Female) %>%
+#   as.matrix() %>%
+#   .[, -1]
+# 
+# rownames(exp_matrix) <- 0:100
+# 
+# ages_unique <- death_matrix %>% row.names %>% as.numeric
+# years_unique <- death_matrix %>% colnames %>% as.numeric
+# 
+# smooth_calculator <- function(row_unique, col_unique, counts, exposure) {
+#   
+#   pre_smoothed <- Mort2Dsmooth(x=row_unique, y=col_unique,
+#                                Z = counts, offset = log(exposure))
+#   
+#   mx <- pre_smoothed$fitted.values/exposure
+#   mx.2 <- mx[, -1]
+#   mx.1 <- mx[, -(ncol(mx))]
+#   aai <- 100 * -log(mx.2/mx.1)
+#   
+#   aai
+# }
+# 
+# smoothed_data <- smooth_calculator(ages_unique, years_unique, death_matrix, exp_matrix)
+# 
+# coh <- years_unique
+# n_coh <- length(years_unique[-1])
+# n_ages <- length(ages_unique)
+# 
+# width_matrix <-
+#   population %>%
+#   select(-Male) %>%
+#   mutate(Female = rescale(Female, c(0, 2))) %>%
+#   spread(Age, Female) %>%
+#   mutate(Cohort = years_unique) %>%
+#   select(Cohort, everything(), -Year) %>%
+#   as.matrix()
+# 
+# stacked_df <-
+#   smoothed_data %>%
+#   as_tibble %>%
+#   gather(year, value) %>%
+#   mutate(age = rep(ages_unique, length(years_unique[-1])))
+# 
+# 
+# library(viridis)
+# library(classInt)
+# colpal <- magma(100, alpha = 1, begin = 0.1, end = 1)
+# # Choose bins
+# bins <- seq(0, 10)
+# # Assigns color according to fixed breaks categorization
+# catg <- classIntervals(stacked_df$value, fixedBreaks=bins, style = "fixed")
+# color <- findColours(catg, colpal)
+# 
+# color_matrix <-
+#   stacked_df %>%
+#   mutate(color = color) %>%
+#   select(year, age, color) %>%
+#   spread(age, color) %>%
+#   as.matrix()
+# 
+# shrink_fun <- function(x, shrink, x_value = TRUE) {
+#   
+#   if(x_value) {
+#     xman <- x
+#     xman[1] <- mean(x[1:2])-(x[2] - x[1])*(shrink/2)
+#     xman[2] <- mean(x[1:2])+(x[2] - x[1])*(shrink/2)
+#   } else {
+#     xman <- x
+#     xman[3] <- mean(x[3:4])-(x[4] - x[3])*(shrink/2)
+#     xman[4] <- mean(x[3:4])+(x[4] - x[3])*(shrink/2)
+#   }
+#   xman
+# }
+# 
+# # The next thing to do is incorporate this smoothe data in the previous plot. This
+# # shouldn't be that hard given that the data is in the same format (matrix, age x year)
+# # as in the previous plot.
+# 
+# # Other thing to do: link both smoothed and previous analysis
+# # to common variables so that both things are estimated from the same
+# # variables.
+# 
+# par(bg = "black", mar=c(5, 4, 4, 2),fig=c(0,1,0,1))
+# 
+# plot(x = c(years_unique[1], years_unique[length(years_unique)]),
+#      y = c(ages_unique[1], ages_unique[length(ages_unique)]),
+#      pch=20,
+#      col="transparent", col.axis=alpha("grey95",0.75),
+#      font.lab=2, cex.lab=1.2, xlab="Year", ylab="Age",
+#      xlim=c(years_unique[1], years_unique[length(years_unique)]), col.lab=alpha("grey95",0.75))
+# 
+# # Loop for cohorts
+# for (i in 1:n_coh) {
+#   # In order to fixate point 2 which we are
+#   # are not shrinking
+#   mid_x <- seq(coh[i],coh[i]+n_ages,1)
+#   mid_y <- c(0:n_ages-1)
+#   
+#   # Loop for ages
+#   for (j in 2:n_ages) {
+#     # Lower Lexis triangle
+#     x <- c(mid_x[j], mid_x[j]+1, mid_x[j]+1, mid_x[j]+1)
+#     y <- c(mid_y[j], mid_y[j], mid_y[j],mid_y[j]+1)
+#     
+#     x_sh <- shrink_fun(x, width_matrix[i, j])
+#     y_sh <- shrink_fun(y, width_matrix[i, j], x_value = F)
+#     
+#     polygon(x_sh, y_sh, lty=0,col=adjustcolor("grey",alpha.f=0.5), border = adjustcolor("grey",alpha.f=0.5))
+#     polygon(x_sh, y_sh, lty=0,col=color_matrix[i, j], border = color_matrix[i, j])
+#     
+#     # Upper Lexis triangle year + 1
+#     x_inv <- c(x[2], x[2] , x[2] ,x[2] + (x[2] - x[1]))
+#     y_inv <- c(y[1],y[4],y[4],y[4])
+#     
+#     x_inv_sh <- shrink_fun(x_inv, width_matrix[i, j], x_value = F)
+#     y_inv_sh <- shrink_fun(y_inv, width_matrix[i, j])
+#     
+#     polygon(x_inv_sh, y_inv_sh, lty=0, col=adjustcolor("grey",alpha.f=0.5), border = adjustcolor("grey",alpha.f=0.5))
+#     polygon(x_inv_sh, y_inv_sh, lty=0, col=color_matrix[i, j], border = color_matrix[i, j])
+#   }
+# }
+# 
+# abline(h=c(seq(0,100,10)),col=alpha("grey95",0.5),lty=2)
+# abline(v=c(seq(1750,2010,10)),col=alpha("grey95",0.5),lty=2)
+# op1 <- par(mar=c(0,0,0,0), fig=c(0.585,0.7,0.035,0.09), new = TRUE)
+# # mtext("Cohort death rates",side=1,line=2,col=alpha("grey95",0.75))
+# plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
+# #text(0.5,0.5,"Cohort mortality rates (cmx)",col=alpha("grey95",0.75))
+# op2 <- par(mar=c(0,0,0,0), fig=c(0.7,0.9,0.05,0.075), new = TRUE)
+# plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
+# lbi <- length(bins)-1
+# for (i in 1:lbi) {
+#   rect(bins[i],0,bins[i+1],1,lty=0,col=colpal[i])
+# }
+# rect(0,0,1,1,lty=1,border=alpha("grey95",0.75))
+# op3 <- par(mar=c(0,0,0,0), fig=c(0.70,0.71,0.020,0.045), new = TRUE)
+# plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
+# text(0.68,0.5,sprintf("%1.0f",0),col=alpha("grey95",0.75))
+# op3 <- par(mar=c(0,0,0,0), fig=c(0.885,0.905,0.020,0.045), new = TRUE)
+# plot(c(0,1),c(0,1),col="transparent",axes=F, xlab="", ylab="")
+# text(0.35,0.5,sprintf("%1.0f",1),col=alpha("grey95",0.75))
+# 
+# # Still need to fix legend scale
+# # Make years automatic because right now you're filtering years manually when reading the dataset.
